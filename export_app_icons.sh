218a04ec7a610065bf13e20197ce6c28d1f87650
andywuest <andreas.wuest.freelancer@gmail.com>
2020-08-18 21:39:37 +0200

    optimize background images

--- export_app_icons.sh
+++ export_app_icons.sh
@@ -5,3 +5,7 @@ do
   inkscape --export-png icons/${x}x${x}/harbour-pollenflug_original.png -w ${x} mediasrc/pollenflug_icon_normal.svg
   pngcrush -brute icons/${x}x${x}/harbour-pollenflug_original.png icons/${x}x${x}/harbour-pollenflug.png
 done
+
+pngcrush -brute mediasrc/pollenflug_black.png qml/icons/background-black.png
+pngcrush -brute mediasrc/pollenflug_white.png qml/icons/background.png
+

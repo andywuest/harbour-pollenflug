4a25f18c037890249ca000611aac8c8a485fda5b
andywuest <andreas.wuest.freelancer@gmail.com>
2020-08-19 20:39:01 +0200

    fixed de translations

--- translations/harbour-pollenflug-de.ts
+++ translations/harbour-pollenflug-de.ts
@@ -8,122 +8,122 @@
         <extracomment>AboutPage title - header
 ----------
 AboutPage title - about text title</extracomment>
-        <translation type="Über Pollenflug"></translation>
+        <translation></translation>
     </message>
     <message>
         <source>Version</source>
         <extracomment>AboutPage version label</extracomment>
-        <translation type="Version"></translation>
+        <translation></translation>
     </message>
     <message>
         <source>Author</source>
         <extracomment>AboutPage author label</extracomment>
-        <translation type="Autor"></translation>
+        <translation>Autor</translation>
     </message>
     <message>
         <source>Source code</source>
         <extracomment>AboutPage about source label</extracomment>
-        <translation type="Quellcode"></translation>
+        <translation>Quellcode</translation>
     </message>
     <message>
         <source>Icons</source>
         <extracomment>AboutPage icon source label</extracomment>
-        <translation type="Icons"></translation>
+        <translation></translation>
     </message>
     <message>
         <source>If you suffer from aan allergy you can watch the pollen situation in your area for various allergenes. Pollenflug is open source and licensed under the GPL v3.</source>
         <extracomment>AboutPage text - about text</extracomment>
-        <translation type="Wenn Sie an einer Allergie leiden, können Sie die Pollensituation in Ihrer Nähe auf verschiedene Allergene beobachten. Pollenflug ist Open Source und unter der GPL v3 lizenziert."></translation>
+        <translation>Wenn Sie an einer Allergie leiden, können Sie die Pollensituation in Ihrer Nähe auf verschiedene Allergene beobachten. Pollenflug ist Open Source und unter der GPL v3 lizenziert.</translation>
     </message>
     <message>
         <source>Contributors</source>
         <extracomment>AboutPage contributors label</extracomment>
-        <translation type="Mitwirkende"></translation>
+        <translation>Mitwirkende</translation>
     </message>
 </context>
 <context>
     <name>CoverPage</name>
     <message>
         <source>Loading...</source>
-        <translation type="Lade..."></translation>
+        <translation>Lade...</translation>
     </message>
     <message>
         <source>Today</source>
-        <translation type="Heute"></translation>
+        <translation>Heute</translation>
     </message>
     <message>
         <source>Tomorrow</source>
-        <translation type="Morgen"></translation>
+        <translation>Morgen</translation>
     </message>
 </context>
 <context>
     <name>GermanPollenBackend</name>
     <message>
         <source>Ambrosia</source>
-        <translation type="Ambrosia"></translation>
+        <translation></translation>
     </message>
     <message>
         <source>Mugwort</source>
-        <translation type="Beifuß"></translation>
+        <translation>Beifuß</translation>
     </message>
     <message>
         <source>Birch</source>
-        <translation type="Birke"></translation>
+        <translation>Birke</translation>
     </message>
     <message>
         <source>Alder</source>
-        <translation type="Erle"></translation>
+        <translation>Erle</translation>
     </message>
     <message>
         <source>Ash Tree</source>
-        <translation type="Esche"></translation>
+        <translation>Esche</translation>
     </message>
     <message>
         <source>Grass</source>
-        <translation type="Gräser"></translation>
+        <translation>Gräser</translation>
     </message>
     <message>
         <source>Hazel</source>
-        <translation type="Hasel"></translation>
+        <translation>Hasel</translation>
     </message>
     <message>
         <source>Rye</source>
-        <translation type="Roggen"></translation>
+        <translation>Roggen</translation>
     </message>
     <message>
         <source>no pollen exposure</source>
-        <translation type="keine Belastung"></translation>
+        <translation>keine Belastung</translation>
     </message>
     <message>
         <source>none to small pollen exposure</source>
-        <translation type="keine bis geringe Belastung"></translation>
+        <translation>keine bis geringe Belastung</translation>
     </message>
     <message>
         <source>small pollen exposure</source>
-        <translation type="geringe Belastung"></translation>
+        <translation>geringe Belastung</translation>
     </message>
     <message>
         <source>small to medium pollen exposure</source>
-        <translation type="geringe bis mittlere Belastung"></translation>
+        <translation>geringe bis mittlere Belastung</translation>
     </message>
     <message>
         <source>medium pollen exposure</source>
-        <translation type="mittlere Belastung"></translation>
+        <translation>mittlere Belastung</translation>
     </message>
     <message>
         <source>medium to high pollen exposure</source>
-        <translation type="mittlere bis hohe Belastung"></translation>
+        <translation>mittlere bis hohe Belastung</translation>
     </message>
     <message>
         <source>high pollen exposure</source>
-        <translation type="hohe Belastung"></translation>
+        <translation>hohe Belastung</translation>
     </message>
 </context>
 <context>
     <name>LoadingIndicator</name>
     <message>
         <source>Loading...</source>
-        <translation type="Lade..."></translation>
+        <translation>Lade...</translation>
     </message>
 </context>
 <context>
@@ -131,37 +131,37 @@ AboutPage title - about text title</extracomment>
     <message>
         <source>About</source>
         <extracomment>OverviewPage about menu item</extracomment>
-        <translation type="Über"></translation>
+        <translation>Über</translation>
     </message>
     <message>
         <source>Settings</source>
         <extracomment>OverviewPage settings menu item</extracomment>
-        <translation type="Einstellungen"></translation>
+        <translation>Einstellungen</translation>
     </message>
     <message>
         <source>Refresh</source>
         <extracomment>OverviewPage refrehs menu item</extracomment>
-        <translation type="Aktualisierung"></translation>
+        <translation>Aktualisierung</translation>
     </message>
     <message>
         <source>Allergen</source>
         <extracomment>OverviewPage header</extracomment>
-        <translation type="Allergen"></translation>
+        <translation></translation>
     </message>
     <message>
         <source>No data available - please configure your state and region properly.</source>
-        <translation type="Keine Daten verfügbar - bitte konfigurieren Sie Ihr Bundesland und Ihre Region richtig"></translation>
+        <translation>Keine Daten verfügbar - bitte konfigurieren Sie Ihr Bundesland und Ihre Region richtig.</translation>
     </message>
 </context>
 <context>
     <name>PollenDateRow</name>
     <message>
         <source>Today</source>
-        <translation type="Heute"></translation>
+        <translation>Heute</translation>
     </message>
     <message>
         <source>Tomorrow</source>
-        <translation type="Morgen"></translation>
+        <translation>Morgen</translation>
     </message>
 </context>
 <context>
@@ -176,218 +176,218 @@ AboutPage title - about text title</extracomment>
     <message>
         <source>Settings</source>
         <extracomment>SettingsPage settings title</extracomment>
-        <translation type="Einstellungen"></translation>
+        <translation>Einstellungen</translation>
     </message>
     <message>
         <source>Schleswig-Holstein und Hamburg</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <source>Mecklenburg-Vorpommern</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <source>Niedersachsen und Bremen</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <source>Allergen</source>
-        <translation type="Allergen"></translation>
+        <translation></translation>
     </message>
     <message>
         <source>Select the region where you live</source>
         <extracomment>SettingsPage part region description</extracomment>
-        <translation type="Wählen Sie die Region aus"></translation>
+        <translation>Wählen Sie die Region aus</translation>
     </message>
     <message>
         <source>Nordrhein-Westfalen</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <source>Brandenburg und Berlin</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <source>Sachsen-Anhalt</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <source>Thüringen</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <source>Sachsen</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <source>Hessen</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <source>Rheinland-Pfalz und Saarland</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <source>Baden-Württemberg</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <source>Bayern</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <source>Select the state where you live</source>
         <extracomment>SettingsPage region description</extracomment>
-        <translation type="Wählen Sie Ihr Bundesland"></translation>
+        <translation>Wählen Sie Ihr Bundesland</translation>
     </message>
     <message>
         <source>Location</source>
-        <translation type="Ort"></translation>
+        <translation>Ort</translation>
     </message>
     <message>
         <source>State</source>
         <extracomment>SettingsPage state</extracomment>
-        <translation type="Bundesland"></translation>
+        <translation>Bundesland</translation>
     </message>
     <message>
         <source>Region</source>
         <extracomment>SettingsPage part region</extracomment>
-        <translation type="Region"></translation>
+        <translation>Region</translation>
     </message>
 </context>
 <context>
     <name>constants</name>
     <message>
         <source>Ambrosia</source>
-        <translation type="Ambrosia"></translation>
+        <translation></translation>
     </message>
     <message>
         <source>Inseln und Marschen</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <source>Geest, Schleswig-Holstein und Hamburg</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <source>Westl. Niedersachsen/Bremen</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <source>Östl. Niedersachsen</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <source>Rhein.-Westfäl. Tiefland</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <source>Mittelgebirge NRW</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <source>Ostwestfalen</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <source>Tiefland Sachsen-Anhalt</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <source>Harz</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <source>Tiefland Thüringen</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <source>Mittelgebirge Thüringen</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <source>Tiefland Sachsen</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <source>Mittelgebirge Sachsen</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <source>Nordhessen und hess. Mittelgebirge</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <source>Rhein-Main</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <source>Rhein, Pfalz, Nahe und Mosel</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <source>Mittelgebirgsbereich Rheinland-Pfalz</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <source>Saarland</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <source>Oberrhein und unteres Neckartal</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <source>Hohenlohe/mittlerer Neckar/Oberschwaben</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <source>Mittelgebirge Baden-Württemberg</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <source>Allgäu/Oberbayern/Bay. Wald</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <source>Donauniederungen</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <source>Bayern n. der Donau, o. Bayr. Wald, o. Mainfranken</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <source>Mainfranken</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <source>Mugwort</source>
-        <translation type="Beifuß"></translation>
+        <translation>Beifuß</translation>
     </message>
     <message>
         <source>Birch</source>
-        <translation type="Birke"></translation>
+        <translation>Birke</translation>
     </message>
     <message>
         <source>Alder</source>
-        <translation type="Erle"></translation>
+        <translation>Erle</translation>
     </message>
     <message>
         <source>Ash Tree</source>
-        <translation type="Esche"></translation>
+        <translation>Esche</translation>
     </message>
     <message>
         <source>Grass</source>
-        <translation type="Gräser"></translation>
+        <translation>Gräser</translation>
     </message>
     <message>
         <source>Hazel</source>
-        <translation type="Hasel"></translation>
+        <translation>Hasel</translation>
     </message>
     <message>
         <source>Rye</source>
-        <translation type="Roggen"></translation>
+        <translation>Roggen</translation>
     </message>
 </context>
 </TS>

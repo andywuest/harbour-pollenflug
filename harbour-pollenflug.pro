b6203da7c0a0d3f36ec5467f616a2b86a4a428f8
andywuest <andreas.wuest.freelancer@gmail.com>
2020-08-30 10:52:03 +0200

    added french translation file;

--- harbour-pollenflug.pro
+++ harbour-pollenflug.pro
@@ -68,7 +68,8 @@ CONFIG += sailfishapp_i18n
 # planning to localize your app, remember to comment out the
 # following TRANSLATIONS line. And also do not forget to
 # modify the localized app name in the the .desktop file.
-TRANSLATIONS += translations/harbour-pollenflug-de.ts
+TRANSLATIONS += translations/harbour-pollenflug-de.ts \
+    translations/harbour-pollenflug-fr.ts
 
 HEADERS += \
     src/constants.h \
---
+++ translations/harbour-pollenflug-fr.ts
@@ -0,0 +1,402 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>AboutPage</name>
+    <message>
+        <source>About Pollenflug</source>
+        <extracomment>AboutPage title - header
+----------
+AboutPage title - about text title</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Version</source>
+        <extracomment>AboutPage version label</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Author</source>
+        <extracomment>AboutPage author label</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Source code</source>
+        <extracomment>AboutPage about source label</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Icons</source>
+        <extracomment>AboutPage icon source label</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>If you suffer from aan allergy you can watch the pollen situation in your area for various allergenes. Pollenflug is open source and licensed under the GPL v3.</source>
+        <extracomment>AboutPage text - about text</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Contributors</source>
+        <extracomment>AboutPage contributors label</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Translators</source>
+        <extracomment>AboutPage translators label</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>monkeyisland1992 (de)</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>CoverPage</name>
+    <message>
+        <source>Loading...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Today</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Tomorrow</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>GermanPollenBackend</name>
+    <message>
+        <source>Ambrosia</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Mugwort</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Birch</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Alder</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Ash Tree</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Grass</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Hazel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Rye</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>no pollen exposure</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>none to small pollen exposure</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>small pollen exposure</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>small to medium pollen exposure</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>medium pollen exposure</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>medium to high pollen exposure</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>high pollen exposure</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>LoadingIndicator</name>
+    <message>
+        <source>Loading...</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>OverviewPage</name>
+    <message>
+        <source>About</source>
+        <extracomment>OverviewPage about menu item</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <extracomment>OverviewPage settings menu item</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Refresh</source>
+        <extracomment>OverviewPage refrehs menu item</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Allergen</source>
+        <extracomment>OverviewPage header</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>No data available - please configure your state and region properly.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>PollenDateRow</name>
+    <message>
+        <source>Today</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Tomorrow</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>PollenPollutionNotUpToDateRow</name>
+    <message>
+        <source>Nicht aktuell</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SettingsPage</name>
+    <message>
+        <source>Settings</source>
+        <extracomment>SettingsPage settings title</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Schleswig-Holstein und Hamburg</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Mecklenburg-Vorpommern</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Niedersachsen und Bremen</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Allergen</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Select the region where you live</source>
+        <extracomment>SettingsPage part region description</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Nordrhein-Westfalen</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Brandenburg und Berlin</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Sachsen-Anhalt</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Thüringen</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Sachsen</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Hessen</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Rheinland-Pfalz und Saarland</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Baden-Württemberg</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Bayern</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Select the state where you live</source>
+        <extracomment>SettingsPage region description</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Location</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>State</source>
+        <extracomment>SettingsPage state</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Region</source>
+        <extracomment>SettingsPage part region</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>constants</name>
+    <message>
+        <source>Ambrosia</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Inseln und Marschen</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Geest, Schleswig-Holstein und Hamburg</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Westl. Niedersachsen/Bremen</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Östl. Niedersachsen</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Rhein.-Westfäl. Tiefland</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Mittelgebirge NRW</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Ostwestfalen</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Tiefland Sachsen-Anhalt</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Harz</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Tiefland Thüringen</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Mittelgebirge Thüringen</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Tiefland Sachsen</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Mittelgebirge Sachsen</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Nordhessen und hess. Mittelgebirge</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Rhein-Main</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Rhein, Pfalz, Nahe und Mosel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Mittelgebirgsbereich Rheinland-Pfalz</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Saarland</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Oberrhein und unteres Neckartal</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Hohenlohe/mittlerer Neckar/Oberschwaben</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Mittelgebirge Baden-Württemberg</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Allgäu/Oberbayern/Bay. Wald</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Donauniederungen</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Bayern n. der Donau, o. Bayr. Wald, o. Mainfranken</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Mainfranken</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Mugwort</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Birch</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Alder</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Ash Tree</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Grass</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Hazel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Rye</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>

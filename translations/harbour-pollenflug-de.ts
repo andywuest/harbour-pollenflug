b8986843000e8c331d2d3ac35639ea14ae13ddff
andywuest <andreas.wuest.freelancer@gmail.com>
2021-03-25 21:02:20 +0100

    various french / german pollen backend changes
    * translation updates;

--- translations/harbour-pollenflug-de.ts
+++ translations/harbour-pollenflug-de.ts
@@ -66,6 +66,57 @@ AboutPage title - about text title</extracomment>
     </message>
 </context>
 <context>
+    <name>FrenchPollenBackend</name>
+    <message>
+        <source>Mugwort</source>
+        <translation>Beifuß</translation>
+    </message>
+    <message>
+        <source>Birch</source>
+        <translation>Birke</translation>
+    </message>
+    <message>
+        <source>Alder</source>
+        <translation>Erle</translation>
+    </message>
+    <message>
+        <source>Grass</source>
+        <translation>Gräser</translation>
+    </message>
+    <message>
+        <source>Hazel</source>
+        <translation>Hasel</translation>
+    </message>
+    <message>
+        <source>Ambrosia</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>no pollen exposure</source>
+        <translation>keine Belastung</translation>
+    </message>
+    <message>
+        <source>very small pollen exposure</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>small pollen exposure</source>
+        <translation>geringe Belastung</translation>
+    </message>
+    <message>
+        <source>medium pollen exposure</source>
+        <translation>mittlere Belastung</translation>
+    </message>
+    <message>
+        <source>high pollen exposure</source>
+        <translation>hohe Belastung</translation>
+    </message>
+    <message>
+        <source>very high pollen exposure</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
     <name>GermanPollenBackend</name>
     <message>
         <source>Ambrosia</source>
@@ -181,11 +232,413 @@ AboutPage title - about text title</extracomment>
     </message>
 </context>
 <context>
-    <name>SettingsPage</name>
+    <name>SettingsFrance</name>
     <message>
-        <source>Settings</source>
-        <extracomment>SettingsPage settings title</extracomment>
-        <translation>Einstellungen</translation>
+        <source>Departement</source>
+        <extracomment>SettingsPage state</extracomment>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Select the departement where you live</source>
+        <extracomment>SettingsPage region description</extracomment>
+        <translation>Wählen Sie das Departement in dem Sie leben.</translation>
+    </message>
+    <message>
+        <source>Ain</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Aisne</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Allier</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Alpes-de-Haute-Provence</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Hautes-Alpes</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Alpes-Maritimes</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Ardèche</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Ardennes</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Ariège</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Aube</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Aude</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Aveyron</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Bouches-du-Rhône</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Calvados</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Cantal</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Charente</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Charente-Maritime</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Cher</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Corrèze</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Corse</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Côte-d&apos;Or</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Côtes-d&apos;Armor</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Creuse</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Dordogne</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Doubs</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Drôme</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Eure</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Eure-et-Loir</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Finistère</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Gard</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Haute-Garonne</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Gers</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Gironde</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Hérault</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Ille-et-Vilaine</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Indre</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Indre-et-Loire</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Isère</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Jura</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Landes</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Loir-et-Cher</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Loire</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Haute-Loire</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Loire-Atlantique</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Loiret</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Lot</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Lot-et-Garonne</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Lozère</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Maine-et-Loire</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Manche</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Marne</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Haute-Marne</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Mayenne</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Meurthe-et-Moselle</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Meuse</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Morbihan</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Moselle</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Nièvre</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Nord</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Oise</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Orne</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Pas-de-Calais</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Puy-de-Dôme</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Pyrénées-Atlantiques</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Hautes-Pyrénées</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Pyrénées-Orientales</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Bas-Rhin</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Haut-Rhin</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Rhône</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Haute-Saône</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Saône-et-Loire</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Sarthe</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Savoie</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Haute-Savoie</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Paris</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Seine-Maritime</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Seine-et-Marne</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Yvelines</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Deux-Sèvres</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Somme</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Tarn</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Tarn-et-Garonne</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Var</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Vaucluse</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Vendée</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Vienne</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Haute-Vienne</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Vosges</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Yonne</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Territoire de Belfort</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Essonne</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Hauts-de-Seine</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Seine-Saint-Denis</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Val-de-Marne</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Val-d&apos;Oise</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Andorra</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SettingsGermany</name>
+    <message>
+        <source>State</source>
+        <extracomment>SettingsPage state</extracomment>
+        <translation>Bundesland</translation>
+    </message>
+    <message>
+        <source>Select the state where you live</source>
+        <extracomment>SettingsPage region description</extracomment>
+        <translation>Wählen Sie Ihr Bundesland</translation>
     </message>
     <message>
         <source>Schleswig-Holstein und Hamburg</source>
@@ -198,15 +651,6 @@ AboutPage title - about text title</extracomment>
     <message>
         <source>Niedersachsen und Bremen</source>
         <translation></translation>
-    </message>
-    <message>
-        <source>Allergen</source>
-        <translation></translation>
-    </message>
-    <message>
-        <source>Select the region where you live</source>
-        <extracomment>SettingsPage part region description</extracomment>
-        <translation>Wählen Sie die Region aus</translation>
     </message>
     <message>
         <source>Nordrhein-Westfalen</source>
@@ -245,23 +689,48 @@ AboutPage title - about text title</extracomment>
         <translation></translation>
     </message>
     <message>
-        <source>Select the state where you live</source>
-        <extracomment>SettingsPage region description</extracomment>
-        <translation>Wählen Sie Ihr Bundesland</translation>
+        <source>Region</source>
+        <extracomment>SettingsPage part region</extracomment>
+        <translation>Region</translation>
+    </message>
+    <message>
+        <source>Select the region where you live</source>
+        <extracomment>SettingsPage part region description</extracomment>
+        <translation>Wählen Sie die Region aus</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsPage</name>
+    <message>
+        <source>Settings</source>
+        <extracomment>SettingsPage settings title</extracomment>
+        <translation>Einstellungen</translation>
     </message>
     <message>
         <source>Location</source>
         <translation>Ort</translation>
     </message>
     <message>
-        <source>State</source>
+        <source>Country</source>
         <extracomment>SettingsPage state</extracomment>
-        <translation>Bundesland</translation>
+        <translation>Land</translation>
     </message>
     <message>
-        <source>Region</source>
-        <extracomment>SettingsPage part region</extracomment>
-        <translation>Region</translation>
+        <source>Select the country where you live</source>
+        <extracomment>SettingsPage region description</extracomment>
+        <translation>Wählen Sie das Land in dem Sie leben</translation>
+    </message>
+    <message>
+        <source>Germany</source>
+        <translation>Deutschland</translation>
+    </message>
+    <message>
+        <source>France</source>
+        <translation>Frankreich</translation>
+    </message>
+    <message>
+        <source>Allergen</source>
+        <translation></translation>
     </message>
 </context>
 <context>
